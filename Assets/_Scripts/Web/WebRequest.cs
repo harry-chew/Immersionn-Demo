d443c6305133115405e72f0b2ef6e5f22ce254dd
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-04-10 13:13:14 +0100

    feat!: retrieve info from server
    created php script that displays date/time
    access this php script from within unity to display date/time in console for now
    
    this will be used to pull video links and other media into engine based on user/login details or whatever

---
+++ Assets/_Scripts/Web/WebRequest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebRequest : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://localhost/immersionn-demo/GetDate.php");
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+}
